e3d97bcf18ceabedc4549f61636d8e39a80879ef
Arunpandian2306 <arunpandian2306@gmail.com>
2024-10-18 21:45:46 +0530

    added binary search

--- routes/Algorithm/Searching.js
+++ routes/Algorithm/Searching.js
@@ -1,31 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-
 function linearSearch(arr, target) {
-    const linear = [];
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        if (arr[i] === target) {
-            linear.push(i);
-        }
+  const linear = [];
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (arr[i] === target) {
+      linear.push(i);
     }
-    return linear;
+  }
+  return linear;
 }
-router.post('/linearSearch', (req, res) => {
+
+function binarySearch(arr, target) {
+  const index = [];
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      index.push(mid);
+    }
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+router.post("/linearSearch", (req, res) => {
+  const { num, target } = req.body;
+  if (!num || !Array.isArray(num)) {
+    res.status(422).json({ error: "Invalid Format" });
+  }
+  const search = linearSearch(num, target);
+  if (search.length === 0) {
+    res.json({ message: "Target Value not found" });
+  } else {
+    res.json({ message: `Target value found at index ${search}` });
+  }
+
+  router.post("/binarySearch", (req, res) => {
     const { num, target } = req.body;
     if (!num || !Array.isArray(num)) {
-        res.status(422).json({ error: "Invalid Format" })
+      res.status(422).json({ error: "Invalid Format" });
     }
-    const search = linearSearch(num, target)
-    if (search.length === 0) {
-        res.json({ message: "Target Value not found" })
+    const sortedArray = num.sort((a, b) => a - b); 
+    const search = binarySearch(sortedArray, target);
+    if (search.lenth > 0) {
+      res.json({ message: `Target value found at index ${index}` });
     } else {
-        res.json({ message: `Target value found at index ${search}` })
+      res.json({ message: `Target Value not found` });
     }
+  });
+});
 
-}
-)
-
-
-module.exports = router
+module.exports = router;
