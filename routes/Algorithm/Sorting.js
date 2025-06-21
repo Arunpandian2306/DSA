1730da85767b2cf87b87d942752b1726d0a4070e
Arun <98469055+Arunpandian2306@users.noreply.github.com>
2025-06-21 18:46:27 +0530

    using temp variable

--- routes/Algorithm/Sorting.js
+++ routes/Algorithm/Sorting.js
@@ -6,7 +6,9 @@ function bubble(arr) {
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                const temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]=temp;
             }
         }
     }
