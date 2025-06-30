078e93734fddcdf7c6ab4d5b9487891791b770ff
Arunpandian2306 <arunpandian2306@gmail.com>
2025-06-30 19:33:49 +0530

    added rev num

--- routes/Programs/Reverse.js
+++ routes/Programs/Reverse.js
@@ -9,4 +9,16 @@ function reversestr(n) {
   return rev;
 }
 
-console.log("reverse>>>", reversestr("arunpandian"));
+console.log("reverse>>>", reversestr("arunpandian"));  //naidnapnura
+
+// reverse the given number
+
+function numrev(n) {
+  const str = n.toString();
+  let rev = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev = rev + str[i];
+  }
+  return Number(rev);
+}
+console.log("numrev>>>", numrev(12345));   //54321
