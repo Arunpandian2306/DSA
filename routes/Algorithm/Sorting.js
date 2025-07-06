f949a6c9aee11c254c126c98cd9be342eeebabd9
Arunpandian2306 <arunpandian2306@gmail.com>
2025-07-06 20:59:56 +0530

    format file

--- routes/Algorithm/Sorting.js
+++ routes/Algorithm/Sorting.js
@@ -1,18 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 function bubble(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j]= arr[j+1];
-                arr[j+1]=temp;
-            }
-        }
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
-    return arr;
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
@@ -30,7 +30,8 @@ function mergeSort(arr) {
 
 function merge(left, right) {
   const result = [];
-  let i = 0, j = 0;
+  let i = 0,
+    j = 0;
   while (i < left.length && j < right.length) {
     if (left[i] < right[j]) {
       result.push(left[i]);
@@ -43,24 +44,22 @@ function merge(left, right) {
   return result.concat(left.slice(i)).concat(right.slice(j));
 }
 
+router.post("/bubbleSort", (req, res) => {
+  const { num } = req.body;
+  if (!num || !Array.isArray(num)) {
+    res.status(422).json({ error: "invalid format" });
+  }
+  const sortelement = bubble(num);
+  res.json({ sortelement });
+});
 
+router.post("/mergeSort", (req, res) => {
+  const { num } = req.body;
+  if (!num || !Array.isArray(num)) {
+    res.status(422).json({ error: "invalid format" });
+  }
+  const sortelement = mergeSort(num);
+  res.json({ sortelement });
+});
 
-router.post('/bubbleSort', (req, res) => {
-    const { num } = req.body;
-    if (!num || !Array.isArray(num)) {
-        res.status(422).json({ error: "invalid format" });
-    }
-    const sortelement = bubble(num);
-    res.json({ sortelement });
-})
-
-router.post('/mergeSort', (req, res) => {
-    const { num } = req.body;
-    if (!num || !Array.isArray(num)) {
-        res.status(422).json({ error: "invalid format" });
-    }
-    const sortelement = mergeSort(num);
-    res.json({ sortelement });
-})
-
-module.exports = router
+module.exports = router;
