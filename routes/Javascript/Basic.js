63887146541f63563437fcf035e97d7c47e193bf
Arunpandian <arunpandian.jayabaskaran@lystloc.com>
2026-05-28 10:30:11 +0530

    basic

---
+++ routes/Javascript/Basic.js
@@ -0,0 +1,13 @@
+const readlineSync = require('readline-sync');
+let num = readlineSync.question("Enter the string");
+
+function reversestr(n) {
+  const str = n.toString();
+  let rev = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev = rev + str[i];
+  }
+  return rev;
+}
+
+console.log("reverse>>>", reversestr(num));
